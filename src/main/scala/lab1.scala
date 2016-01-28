a0c5f62fdd08b2b91c8a902fbc29e3c47f49877f
Ketaki Borkar <kborkar@us.ibm.com>
2016-01-27 21:57:24 -0800

    more changes

--- src/main/scala/lab1.scala
+++ src/main/scala/lab1.scala
@@ -1,14 +1,22 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.sql._
 import org.apache.spark.SparkConf
+import java.lang.Math
 
 object lab1 {
   def main(args: Array[String]) {
   //set up the Spark configuration
     val conf = new SparkConf().setAppName("LD").setMaster("spark://localhost:7077");
     conf.set("es.index.auto.create", "true");
+    conf.setAppName("lab1")
+   
     val sc = new SparkContext(conf);
+    //IMPORTANT!!
+    //need to run sbt clean package first, so this jar is generated
+    //then add this jar to the sc so it is deployed to all the spark clusters
+    sc.addJar("target/scala-2.10/bigdatauniversity_2.10-1.0.jar")
     val sqlContext = new SQLContext(sc);
+
     
     val readme = sc.textFile("src/main/resources/labfiles/README.md")
     println(readme.count());
@@ -18,8 +26,14 @@ object lab1 {
     println(linesWithSpark);
     val scMap = readme.map(line => line.split(" ").size)
     val scReduce = scMap.reduce((a, b) => if (a > b) a else b)
-    println("map " + readme.map(line => line.split(" ").size));
+    println("map --> " + scMap);
+    //println("reduce -->" + scReduce)
     
+    val mathReduce = readme.map(line => line.split(" ").size).reduce((a, b) => Math.max(a,b))
+    println("mathReduce --->" + mathReduce)
+    
+    val wordCounts = readme.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a,b) => a+b)
+    println("wordCounts --> " + wordCounts);
     sc.stop();
   }
 
